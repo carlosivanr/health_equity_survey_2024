65b294dd2fd0701837f679e65eb371e0bb9f32b4
Rodriguez <carlos.i.rodriguez@cuanschutz.edu>
2024-09-26 16:44:51 -0600

    Updates to plots, tables, and parallel processing file.

--- scripts/generate_all_reports.R
+++ scripts/generate_all_reports.R
@@ -1,74 +1,74 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Carlos Rodriguez Ph.D. CU Anschutz Department of Family Medicine
+# 09/26/2024
+# Render .qmd files in parallel
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-# Load libraries ----
+# Load libraries ---------------------------------------------------------------
 pacman::p_load(tidyverse,
-               here)
+               here,
+               furrr)
 
 # Set the input clinics, output format parameters, and number of cores ----
 clinics = c("AF Williams", "Boulder", "Lone Tree", "Westminster", "WISH", "All")
 format = "docx"
-cores = 6
+cores = 5
 
-# Load create_reports function----
-# Create reports function
+# Create reports function ------------------------------------------------------
 create_reports <- function(clinic_name){
   
-  # Set the input file path, which will be a copy of the *.qmd file
-  layout = "./scripts/layout_by_clinic_v2.qmd"
+  # Relative to the root project directory, set the path to the master layout
+  # .qmd file 
+  layout <-  "./scripts/layout_by_clinic_v3.qmd"
   
-  # Make a copy of the layout .qmd file in the scripts directory as the
-  # reference document has a relative path from this directory
+  # Create a clinic-specific .qmd file. Copy and rename the master layout .qmd
+  # file in the scripts directory removing spaces in the clinic_name to avoid
+  # rendering issues, and allow parallel processing because the same file can't 
+  # be read in multiple future sessions in parallel.
   file.copy(
     from = layout,
-    to = here("scripts", str_c(clinic_name, ".qmd")),
+    to = here("scripts", str_c(sub(" ", "", clinic_name), ".qmd")),
     overwrite = TRUE
   )
   
-  # Set the file_in to the copied .qmd file
-  file_in = here("scripts", str_c(clinic_name, ".qmd"))
+  # Set file_in to the copied and renamed clinic-specific .qmd file. Serves as
+  # an input to the quarto_render() function
+  file_in <- here("scripts", str_c(sub(" ", "", clinic_name), ".qmd"))
   
-  # Render an output given the copied input_file and clinic_name
+  # Set file_out to the file name of the rendered report. Serves as a parameter
+  # in the output-file option of the clinic-specific .qmd file.
+  file_out <- str_c(clinic_name, "." ,format)
+  
+  # Render the report
   quarto::quarto_render(
     input = file_in,
     execute_params = list(clinic = clinic_name),
-    output_format = "docx"
+    output_format = "docx",
+    output_file = file_out
   )
   
-  # Move the file to the reports directory
+  # The reports are initially placed in the root project directory. Copy the 
+  # output .docx file to the reports directory
   file.copy(
-    from = file_in,
-    to = here("deliverables/reports", str_c(clinic_name, ".docx")),
+    from = here(file_out),
+    to = here("deliverables/reports", str_c(clinic_name, ".", format)),
     overwrite = TRUE
   )
   
-  # Remove the file_in .qmd file and the docx file
-  file.remove(here("scripts", str_c(clinic_name, ".docx")))
-  file.remove(here("scripts", str_c(clinic_name, ".qmd")))
+  # Remove the output .docx file from the root project directory. Uses a
+  # relative path
+  file.remove(here(file_out))
   
+  # Remove the copy of the master layout .qmd file. Uses an absolute Path
+  file.remove(file_in)
   
-  # AF Williams and Lone Tree results in intermediate directories that are not
-  # removed due to some issue with the space in the name of the clinic. This
-  # code chunk was written to remove any leftover directories
-  # files_dir <- str_c("./scripts/layout_", 
-  #                    format, "_", 
-  #                    sub(" ", "-", clinic_name), 
-  #                    "_files"
-  # )
-  # 
-  # if (dir.exists(files_dir)){
-  #   unlink(files_dir, recursive = TRUE)
-  # }
 }
 
-# Set the options for furrr----
-# options(future.rng.onMisuse = "ignore")
-# plan(multisession, workers = cores)
+# Set furrr options ------------------------------------------------------------
+options(future.rng.onMisuse = "ignore")
+plan(multisession, workers = cores)
 
-# Run the create_reports() function in parallel to all input clinics----
-# system.time(clinics %>%
-#               future_walk(~ create_reports(.x))
-# )
-
-# # Run in serial
+# Render reports in parallel ---------------------------------------------------
 system.time(clinics %>%
-              walk(~ create_reports(.x))
+              future_walk(~ create_reports(.x))
 )
