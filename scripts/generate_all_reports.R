cb5781eecaf7448d33611820e331366f6e9538d9
rodrica2 <carlos.i.rodriguez@cuanschutz.edu>
2024-10-01 17:18:15 -0600

    Updates to add clinic specific response rates and 2023 vs 2024

--- scripts/generate_all_reports.R
+++ scripts/generate_all_reports.R
@@ -1,6 +1,6 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Carlos Rodriguez Ph.D. CU Anschutz Department of Family Medicine
-# 09/26/2024
+# 09-26-2024
 # Render .qmd files in parallel
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -10,35 +10,36 @@ pacman::p_load(tidyverse,
                furrr)
 
 # Set the input clinics, output format parameters, and number of cores ---------
-clinics = c("AF Williams", "Boulder", "Lone Tree", "Westminster", "WISH", "All")
-format = "docx"
-cores = 5
+clinics <- c("AF Williams", "Boulder", "Lone Tree",
+             "Westminster", "WISH", "All")
+format <- "docx"
+cores <- 5
 
 # Create reports function ------------------------------------------------------
-create_reports <- function(clinic_name){
-  
+create_reports <- function(clinic_name) {
+
   # Relative to the root project directory, set the path to the master layout
-  # .qmd file 
-  layout <-  "./scripts/layout_by_clinic_v3.qmd"
-  
+  # .qmd file
+  layout <-  "./scripts/layout_by_clinic_v4.qmd"
+
   # Create a clinic-specific .qmd file. Copy and rename the master layout .qmd
   # file in the scripts directory removing spaces in the clinic_name to avoid
-  # rendering issues, and allow parallel processing because the same file can't 
+  # rendering issues, and allow parallel processing because the same file can't
   # be read in multiple future sessions in parallel.
   file.copy(
     from = layout,
     to = here("scripts", str_c(sub(" ", "", clinic_name), ".qmd")),
     overwrite = TRUE
   )
-  
+
   # Set file_in to the copied and renamed clinic-specific .qmd file. Serves as
   # an input to the quarto_render() function
   file_in <- here("scripts", str_c(sub(" ", "", clinic_name), ".qmd"))
-  
+
   # Set file_out to the file name of the rendered report. Serves as a parameter
   # in the output-file option of the clinic-specific .qmd file.
-  file_out <- str_c(clinic_name, "." ,format)
-  
+  file_out <- str_c(clinic_name, ".", format)
+
   # Render the report
   quarto::quarto_render(
     input = file_in,
@@ -46,22 +47,22 @@ create_reports <- function(clinic_name){
     output_format = "docx",
     output_file = file_out
   )
-  
-  # The reports are initially placed in the root project directory. Copy the 
+
+  # The reports are initially placed in the root project directory. Copy the
   # output .docx file to the reports directory
   file.copy(
     from = here(file_out),
     to = here("deliverables/reports", str_c(clinic_name, ".", format)),
     overwrite = TRUE
   )
-  
+
   # Remove the output .docx file from the root project directory. Uses a
   # relative path
   file.remove(here(file_out))
-  
+
   # Remove the copy of the master layout .qmd file. Uses an absolute Path
   file.remove(file_in)
-  
+
 }
 
 # Set furrr options ------------------------------------------------------------
@@ -69,6 +70,4 @@ options(future.rng.onMisuse = "ignore")
 plan(multisession, workers = cores)
 
 # Render reports in parallel ---------------------------------------------------
-system.time(clinics %>%
-              future_walk(~ create_reports(.x))
-)
+system.time(clinics %>% future_walk(~ create_reports(.x)))
